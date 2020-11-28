ea2325ac2493098fe4712866fc93d00fbd1e33ff
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-28 15:59:53 +0300

    Installed Bootstrap4

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'awards.apps.AwardsConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
