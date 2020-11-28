b73539b6eabd48d0d1dc2b2b674c0bb8a51a50d5
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-28 16:26:56 +0300

    Added a view called home

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile,Project,Rating
+from django.contrib.auth.models import User
+from .forms import ProfileForm,ProjectForm,RatingForm,UserForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def home(request):
+    users = User.objects.exclude(id=request.user.id)
+    projects = Project.all_projects()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,request.FILES)
+
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.profile = request.user.profile
+            project.save()
+
+            return redirect('home')
+    else:
+        form = ProjectForm()
+    return render(request,'awards/home.html',{"users":users,"projects":projects,"form":form})
+
+
