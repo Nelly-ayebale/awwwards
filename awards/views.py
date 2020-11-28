8f5f5aafb56253167b17dd4949f68f0fa165dfbf
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-28 19:20:41 +0300

    Created a view for the profile

--- awards/views.py
+++ awards/views.py
@@ -23,4 +23,21 @@ def home(request):
         form = ProjectForm()
     return render(request,'awards/home.html',{"users":users,"projects":projects,"form":form})
 
+@login_required(login_url='accounts/login/')
+def profile(request, username):
+    projects = request.user.profile.projects.all()
 
+    if request.method == 'POST':
+        user_form = UserForm(request.POST,instance=request.user)
+        profile_form = ProfileForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            return redirect('home')
+    
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'awards/profile.html',{"user_form":user_form,"profile_form":profile_form,"projects":projects})
