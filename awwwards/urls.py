5715ab7fea8f59de609f17904c59fe3371ab795c
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-28 16:41:36 +0300

    Added django_registration and registration templates

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'',include('awards.urls')),
+    path('accounts/register/',RegistrationView.as_view(success_url='/profile/'),name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     
 ]
